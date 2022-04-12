a229162b3646476bd4be1e2e04d5830c87e23c66
maycrxyy <mauricio.munoz.gonzalez@alumnos.sip.cl>
2022-04-12 10:43:30 -0400

    Cambio

---
+++ Mauricio_muñoz_Predecir_arreglos_y_condicionales.js
@@ -0,0 +1,27 @@
+function muestraInformaciónContacto() {
+    var auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo);
+}
+/*
+            Variables			=       Datos
+			auntContactInfo		=        ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345]
+*/
+function muestraListaDeCompras() {
+    var produce = ["manzanas", "naranjas"];
+    var frozen = ["brócoli", "helado"];
+    frozen.push("croqueta de papa");
+    console.log(frozen);
+}
+/*
+		Variables 				Datos
+		produce		=		["manzanas", "naranjas"]
+		frozen 		=		["brócoli", "helado"]
+*/
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+/*
+        Variables		=	    Datos
+		movieLibrary	=	    ["Bambi", "E.T.", "Toy Story"]
+*/  
