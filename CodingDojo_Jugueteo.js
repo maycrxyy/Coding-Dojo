0f1172e2fa96e6daa89503f9ceb0435886b1b82e
maycrxyy <mauricio.munoz.gonzalez@alumnos.sip.cl>
2022-04-21 09:19:58 -0400

    Cambios

---
+++ CodingDojo_Jugueteo.js
@@ -0,0 +1,9 @@
+function MuestraSiElNiñoPuedeSubirALaMontañaRusa(alturaNiño) {
+    if (alturaNiño >52)
+    {
+        console.log("!subete, chico!")
+    }
+    else{
+console.log("lo siento, chico tal vez el próximo año")
+    }
+}
